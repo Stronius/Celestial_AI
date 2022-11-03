750c435ba5ec1f247608cfedaf04996f1ffe1016
Stronius <61824330+Stronius@users.noreply.github.com>
2022-11-03 10:01:48 +0100

    CelestailAI Controller

---
+++ Assets/Teams/Celestial_AI/CelestialAIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoNotModify;
+
+namespace CelestialTeam {
+
+	public class CelestialAIController : BaseSpaceShipController
+	{
+		public override void Initialize(SpaceShipView spaceship, GameData data)
+		{
+		}
+
+		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
+		{
+			SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+			float thrust = 1.0f;
+			float targetOrient = spaceship.Orientation + 90.0f;
+			bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
+			return new InputData(thrust, targetOrient, needShoot, false, false);
+		}
+
+		void UpdateBlackboard()
+        {
+
+        }
+	}
+}
